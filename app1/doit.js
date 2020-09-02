6c9e2d538c4e8ea004cb88d8e599093feba8e1df
Marko Timonen <marko.timonen@lemonsoft.fi>
2020-09-02 10:58:49 +0300

    a

---
+++ app1/doit.js
@@ -0,0 +1,10 @@
+
+            Office.onReady(function() {
+                // Office is ready
+                $(document).ready(function () {
+                    //var element1 = document.getElementById('a');
+        
+                    //element1.innerHTML = "initialize1";
+                    document.write("test");
+                });
+            });
