2e4b3b0c43c910127352617d0b1f853bb2937e02
Marko Timonen <marko.timonen@lemonsoft.fi>
2020-09-02 11:14:22 +0300

    a

--- app1/doit.js
+++ app1/doit.js
@@ -8,5 +8,20 @@
                     //element1.innerHTML = "initialize1";
                     //document.write("test");
                     console.log("test_xxxxxxxxxx");
+
+                            var element1 = document.getElementById('a');
+                            var element2 = document.getElementById('b');
+
+                            element1.innerHTML = "initialize31";
+                            var element1 = document.getElementById('a');
+                            var element2 = document.getElementById('b');
+                            element1.innerHTML = "EWS URL: " + Office.context.mailbox.ewsUrl;
+                            element1.innerHTML = "REST URL: " + Office.context.mailbox.restUrl;
+                            
+                            console.log("REST URL: " + Office.context.mailbox.restUrl);
+                            console.log("EWS URL: " + Office.context.mailbox.ewsUrl);
+
+
+
                 });
             });
